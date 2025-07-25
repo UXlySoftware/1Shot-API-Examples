fff1c6798306fa9e77a4bc2d20b22303eaef130d
TtheBC01 <tac688@alumni.stanford.edu>
2025-07-25 12:08:15 -0700

    discord bot

---
+++ python/discord-bot/README.md
@@ -0,0 +1,45 @@
+
+[![Watch the tutorial](https://img.youtube.com/vi/LIz4ANRdPus/maxresdefault.jpg)](https://youtu.be/LIz4ANRdPus)
+
+# 1Shot Discord Bot Demo
+
+This is a minimal example of how to leverage 1Shot API in a Discord Bot. 
+
+## 1. Create a Discord App
+
+Visit the [Discord application console](https://discord.com/developers/applications) and create a new bot. You'll need to generate a bot token and keep it somewhere safe. 
+
+## 2. Create a 1Shot API Account
+
+Log into 1Shot and create a new API key and secret from the [API Keys](https://app.1shotapi.com/api-keys) page. You'll need both the client ID and
+secret for your bot. 
+
+Also, go to the [Organizations page](https://app.1shotapi.com/organizations) and click on the "Details" button of your org. Grab the business ID from the url. 
+
+## 3. Create a 1Shot API Endpoint
+
+Make sure you have [created and funded](https://app.1shotapi.com/escrow-wallets) an Escrow Wallet on the Sepolia Network. On the [Endpoints](https://app.1shotapi.com/endpoints) page, import [0xA1BfEd6c6F1C3A516590edDAc7A8e359C2189A61](https://sepolia.etherscan.io/address/0xA1BfEd6c6F1C3A516590edDAc7A8e359C2189A61) on the Sepolia testnet. 
+
+Click on the details of the function `deployToken` and the the endpoint Id. 
+
+## 4. Build the bot Docker image
+
+Clone this repo and build the bot container image:
+
+```sh
+docker build -t bot . 
+```
+
+## 5. Run the bot container
+
+Gather your Discord bot token, API key and secret, business id, and endpoint id to run the bot container:
+
+```sh
+docker run -d --rm --name bot -e DISCORD_BOT_TOKEN=... -e API_KEY=... -e API_SECRET=... -e BUSINESS_ID=... -e ENDPOINT_ID=... bot
+```
+
+## 6. Add the bot to your server
+
+Add the bot to you server and invoke `/deploy_token`. 
+
+![Demo of the bot in action](deploy-token.gif)
---
+++ python/discord-bot/bot.py
@@ -0,0 +1,59 @@
+import discord
+from discord.ext import tasks
+import os
+
+from oneshot import get_bearer_token, get_endpoint, call_endpoint
+
+# this is the endpoint id of the deployToken endpoint which will deploy ERC20 tokens
+# you can find the endpoint id in the 1Shot endpoint dashboard
+ENDPOINT_ID = os.getenv("ENDPOINT_ID", "deployToken_endpoint")
+
+access_token = get_bearer_token()
+endpoint = get_endpoint(access_token, ENDPOINT_ID)
+
+bot = discord.Bot()
+
+# create a task to refresh the token every 60(ish) minutes
+@tasks.loop(minutes=55)
+async def refresh_token():
+    access_token = get_bearer_token()
+    print("Token refreshed")
+
+# example of how to create a command with a button
+class MyView(discord.ui.View): # Create a class called MyView that subclasses discord.ui.View
+    @discord.ui.button(label="Click me!", style=discord.ButtonStyle.primary, emoji="😎") # Create a button with the label "😎 Click me!" with color Blurple
+    async def button_callback(self, button, interaction):
+        await interaction.response.send_message("You clicked the button!") # Send a message when the button is clicked
+
+@bot.slash_command() # Create a slash command
+async def button(ctx):
+    await ctx.respond("This is a button!", view=MyView()) # Send a message with our View class that contains the button
+
+# example of a simple command with no arguments
+@bot.command(description="Sends the bot's latency.") # this decorator makes a slash command
+async def ping(ctx): # a slash command will be created with the name "ping"
+    print("Ping command called")
+    await ctx.respond(f"Pong! Latency is {bot.latency}")
+
+# example of a command with arguments that will call 1Shot API
+@bot.command(description="Deploy a token.")
+async def deploy_token(ctx, admin: str, name: str, symbol: str, supply: str):
+    for param in endpoint.params:
+        if param.name == "admin":
+            param.value = admin
+        elif param.name == "name":
+            param.value = name
+        elif param.name == "ticker":
+            param.value = symbol
+        elif param.name == "premint":
+            param.value = supply + "000000000000000000"  # Assuming supply is in wei
+    # Call the endpoint with the updated parameters
+    execution = call_endpoint(access_token, endpoint)
+    await ctx.respond(f"Deploying {name} owned by {admin} with {supply} amount of {symbol}. Transaction Hash: {execution.transactionHash}")
+
+# Replace 'YOUR_BOT_TOKEN' with your actual bot token or set DISCORD_BOT_TOKEN as an environment variable
+TOKEN = os.getenv("DISCORD_BOT_TOKEN", "YOUR_BOT_TOKEN")
+
+if __name__ == "__main__":
+    print("Starting bot...")
+    bot.run(TOKEN)
